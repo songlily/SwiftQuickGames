91dd25ebb005926bff15f9190b423dcfcbcaa6b6
Lily Song <lilysong@Lilys-MBP.lan>
2018-01-05 03:29:28 -0500

    first commit

---
+++ QuickGames/ConnectFourBoard.swift
@@ -0,0 +1,21 @@
+//
+//  ConnectFourBoard.swift
+//  QuickGames
+//
+//  Created by Lily Song on 2018-01-01.
+//  Copyright © 2018 Lily Song. All rights reserved.
+//
+
+import UIKit
+
+class ConnectFourBoard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
